8efe9fbf4e770647ff57f83a36f9f2253ba63d92
Hishaq00 <157884178+Hishaq00@users.noreply.github.com>
2024-11-21 23:36:01 -0800

    Update query.php

--- query.php
+++ query.php
@@ -3,12 +3,17 @@ include('connection.php');
 session_start();
 ?>
 <?php
-if(isset($_POST["AddToCart"])){
-if(isset($_SESSION['cart'])){}
+if(isset($_POST["AddToCart"])){               
+if(isset($_SESSION['cart'])){
+    $count = count($_SESSION['cart']);
+    $_SESSION['cart'][0]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
+    echo "<script>alert('cart added successfully');
+    location.assign('index.php')</script>";
 }
 else{
     $_SESSION['cart'][0]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
     echo "<script>alert('cart added successfully');
     location.assign('index.php')</script>";
+}
 }
 ?>
