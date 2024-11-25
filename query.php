81fdd42e9fe86dd64a4d0bc962239dba632cf895
Hishaq00 <157884178+Hishaq00@users.noreply.github.com>
2024-11-24 23:47:57 -0800

    Update query.php

--- query.php
+++ query.php
@@ -1,17 +1,19 @@
 <?php
 include('connection.php');
 session_start();
+
+
 ?>
 <?php
 if(isset($_POST["AddToCart"])){               
 if(isset($_SESSION['cart'])){
     $count = count($_SESSION['cart']);
-    $_SESSION['cart'][0]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
+    $_SESSION['cart'][$count]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proqty"=>$_POST['pqty'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
     echo "<script>alert('cart added successfully');
     location.assign('index.php')</script>";
 }
 else{
-    $_SESSION['cart'][0]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
+    $_SESSION['cart'][$count]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proqty"=>$_POST['pqty'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
     echo "<script>alert('cart added successfully');
     location.assign('index.php')</script>";
 }
