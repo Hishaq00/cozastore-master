ca6bd7a166695395655b6675e7042286fbd6e042
Hishaq00 <157884178+Hishaq00@users.noreply.github.com>
2024-11-28 18:14:32 +0500

    Update query.php

--- query.php
+++ query.php
@@ -1,21 +1,32 @@
 <?php
 include('connection.php');
 session_start();
-
-
 ?>
 <?php
 if(isset($_POST["AddToCart"])){               
 if(isset($_SESSION['cart'])){
-    $count = count($_SESSION['cart']);
-    $_SESSION['cart'][$count]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proqty"=>$_POST['pqty'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
-    echo "<script>alert('cart added successfully');
-    location.assign('index.php')</script>";
-}
+    // $count = count($_SESSION['cart']);
+    // $_SESSION['cart'][$count]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proqty"=>$_POST['pqty'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
+    // echo "<script>alert('cart added successfully');
+    // location.assign('index.php')</script>";
+        foreach($_SESSION['cart'] as $item) {
+            if($item['proid'] == $_POST['pid']) {
+                $productExists = true;
+                break;
+            }
+        }
+        if($productExists) {
+            echo "<script>alert('Product already exists in the cart!'); location.assign('index.php');</script>";
+        }
+
 else{
     $_SESSION['cart'][$count]=array("proid"=>$_POST['pid'],"proname"=>$_POST['pname'],"proqty"=>$_POST['pqty'],"proprice"=>$_POST['pprice'],"proimg"=>$_POST['pimg']);
     echo "<script>alert('cart added successfully');
     location.assign('index.php')</script>";
 }
-}
+}    }
 ?>
+
+
+
+
